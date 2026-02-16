4299d009f1b1508e9352b51207ab91dbfdbd3f1a
Kaveesha Dias <kaveeshadias27@gmail.com>
2026-02-17 03:03:36 +0530

    Sample 1 test

--- sql_test.py
+++ sql_test.py
@@ -1,9 +1,15 @@
 import pyodbc
-
-connt = pyodbc.Connection(  'Driver={ODBC Driver 17 for SQL Server};'
+ 
+connt = pyodbc.connect(  'Driver={ODBC Driver 17 for SQL Server};' 
                             'Server=.\SQLEXPRESS;'
                             'Database=ExpensesTracker;'
                             'Trusted_Connection=yes;'
                         )
 
 print("Connected successfully")
+
+cursor = connt.execute("SELECT 1")
+
+row = cursor.fetchone() 
+print(row) 
+
