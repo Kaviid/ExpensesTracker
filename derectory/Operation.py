6c00cb0eb8e8f381ae09f7fdb9e2c9d1c8221d03
Kaveesha Dias <kaveeshadias27@gmail.com>
2026-02-24 23:46:48 +0530

    Fix TypeError <updated_balance = self.cursor.fetchone()[0]> when enter amount as the first it add to top row and TID = 1

--- derectory/Operation.py
+++ derectory/Operation.py
@@ -14,36 +14,48 @@ class Operation :
     pass
 
   def Income(self, amount) :
-    quary = "INSERT INTO Statment (TID,date,descreption,income,expenses,balance) VALUES (?,?,?,?,?,?)" #Income add Quary store in var
-    date = datetime.datetime.now().date() #Also date store in date var
+    try :
+      quary = "INSERT INTO Statment (date,descreption,income,expenses,balance) VALUES (?,?,?,?,?)" #Income add Quary store in var
+      date = datetime.datetime.now().date() #Also date store in date var
 
-    #Get current balance and add amount
-    self.cursor.execute("SELECT TOP 1 balance FROM Statment ORDER BY TID DESC;") 
-    updated_balance = self.cursor.fetchone()[0]
-    updated_balance = updated_balance + amount
+      #Get current balance and add amount
+      self.cursor.execute("SELECT TOP 1 balance FROM Statment ORDER BY TID DESC;") 
+      updated_balance = self.cursor.fetchone()[0]
+      updated_balance = updated_balance + amount
 
-    #Insert updated values into DataBase
-    self.cursor.execute(quary, (12, date, "descreption", amount, None, updated_balance))
-    self.connt.commit()
-    print("Insert Syccessfully!")
+      #Insert updated values into DataBase
+      self.cursor.execute(quary, (date, "descreption", amount, None, updated_balance))
+      self.connt.commit()
+      print("Insert Syccessfully!")
+
+    except TypeError:
+      self.cursor.execute(quary, (date, "descreption", amount, None, amount))
+      self.connt.commit()
+      print(f"DataBase empty...Added {amount} as the First!")
 
   def Expenses(self, amount) :
-    quary = "INSERT INTO Statment (TID, date, descreption, income, expenses, balance) VALUES (?,?,?,?,?,?)" #Expenses add Quary store in var
-    date = datetime.datetime.now().date() #Get date
+    try :
+      quary = "INSERT INTO Statment (date, descreption, income, expenses, balance) VALUES (?,?,?,?,?)" #Expenses add Quary store in var
+      date = datetime.datetime.now().date() #Get date
 
-    #Get current balance from the DataBase and substract amount
-    self.cursor.execute("SELECT TOP 1 balance FROM Statment ORDER BY TID DESC;")
-    updated_balance = self.cursor.fetchone()[0]
-    updated_balance = updated_balance - amount
+      #Get current balance from the DataBase and substract amount
+      self.cursor.execute("SELECT TOP 1 balance FROM Statment ORDER BY TID DESC;")
+      updated_balance = self.cursor.fetchone()[0]
+      updated_balance = updated_balance - amount
 
-    #Insert 
-    self.cursor.execute(quary, (13, date, "Descreption", None, amount, updated_balance))
-    self.connt.commit()
-    print("Insert Syccessfully!")
+      #Insert 
+      self.cursor.execute(quary, (date, "Descreption", None, amount, updated_balance))
+      self.connt.commit()
+      print("Insert Syccessfully!")
+
+    except TypeError:
+      self.cursor.execute(quary, (date, "Descreption", None, amount, amount))
+      self.connt.commit()
+      print(f"DataBase empty...Added {amount} as the First!")
 
 
 
 
 test = Operation()
-test.Income(4000)
-test.Expenses(12000.50)
+#test.Income(23000)
+test.Expenses(12000)
