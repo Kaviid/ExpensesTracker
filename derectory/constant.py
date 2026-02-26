74b111a035a7c163977143fb265e6fd95138f646
Kaveesha Dias <kaveeshadias27@gmail.com>
2026-02-27 00:58:43 +0530

    Add categories list in dict

--- derectory/constant.py
+++ derectory/constant.py
@@ -14,3 +14,27 @@ methods_link = {
     '2' : add.expense,
     '3' : add.balance
 }
+
+expense_categories = {
+    "1": "Rent",
+    "2": "Utilities",
+    "3": "Groceries",
+    "4": "Transportation",
+    "6": "Phone & Internet",
+    "7": "Dining Out",
+    "8": "Entertainment",
+    "9": "Shopping",
+    "10": "Gym / Fitness",
+    "11": "Salon / Personal Care",
+    "12": "Health & Medical",
+    "13": "Gifts",
+    "14": "Subscription",
+    "15": "Miscellaneous",
+    "16": "Router",
+    "17": "Phone Call",
+    "18": "Hutch Data",
+    "19": "Fire Extinguisher",
+    "20": "Smile / Saving",
+    "21": "Other"
+}
+
